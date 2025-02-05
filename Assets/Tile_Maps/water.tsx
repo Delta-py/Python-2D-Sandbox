64b1542c57c196e51752f000219d1396bb87b2d7
DeltaPython <nathandcrouch@gmail.com>
2025-02-05 20:31:52 +0000

    add assets

---
+++ Assets/Tile_Maps/water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="water" tilewidth="16" tileheight="16" tilecount="240" columns="60" objectalignment="center">
+ <image source="../Images/Tiles/water.png" width="960" height="64"/>
+</tileset>
