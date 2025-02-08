9c2118de2e735b19b5791895c4f86ae0800a850e
DeltaPython <nathandcrouch@gmail.com>
2025-02-08 21:15:16 +0000

    Added player

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import pygame
 import time
 from settings import *
 
+class Player:
+	def __init__(self):
+		self.position = pygame.math.Vector2(0, 100)
+		self.texture = CHARACTER_TEXTURES['idle_f']
+		self.display = pygame.display.get_surface()
+
+	def update(self, delta_time):
+		self.position.x += delta_time
+
+	def draw(self, total_time):
+		self.display.blit(self.texture[int(total_time * 4) % len(self.texture)], self.position)
+
 class Game:
 	def __init__(self):
 		pygame.init()
@@ -10,21 +22,28 @@ class Game:
 		self.screen = pygame.display.get_surface()
 		self.clock = pygame.time.Clock()
 
+		self.player = Player()
+
+	def update(self, delta_time):
+		self.player.update(delta_time)
+
 	def draw(self, delta_time, total_time):
 		self.screen.fill((0, 0, 0))
-		for x in range(0, int(WINDOW_SIZE.x), TEXTURES['grass'][0].get_width()):
-				for y in range(0, int(WINDOW_SIZE.y), TEXTURES['grass'][0].get_height()):
-					self.screen.blit(TEXTURES['grass'][int(total_time) % len(TEXTURES['grass'])], (x, y))
+		for x in range(0, int(WINDOW_SIZE.x), TILE_TEXTURES['grass'][0].get_width()):
+				for y in range(0, int(WINDOW_SIZE.y), TILE_TEXTURES['grass'][0].get_height()):
+					self.screen.blit(TILE_TEXTURES['grass'][int(total_time * 2) % len(TILE_TEXTURES['grass'])], (x, y))
+		self.player.draw(total_time)
 		pygame.display.update()
 
 	def run(self):
 		running = True
 		while running:
-			delta_time = self.clock.tick(pygame.display.get_current_refresh_rate())
-			total_time = time.gmtime().tm_sec
+			delta_time = 1 / self.clock.tick(pygame.display.get_current_refresh_rate())
+			total_time = time.time()
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					running = False
+			self.update(delta_time)
 			self.draw(delta_time, total_time)
 		pygame.quit()
 
--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
-from pydoc import text
 import pygame
 import os
 
-WINDOW_SIZE = pygame.math.Vector2(16, 9) * 32
+WINDOW_SIZE = pygame.math.Vector2(16, 9) * 16
+PLAYER_SIZE = pygame.math.Vector2(16, 24)
 
 def load_texture(filename, type):
 	texture = pygame.image.load(os.path.join('C:\\', *((__file__.split(':')[1]).split('\\')[:-1]), 'Assets', 'Images', type, f'{filename}.png'))
@@ -15,7 +15,19 @@ def load_tile_animation(tile):
 		animation_frames.append(texture.subsurface((i * texture.get_height(), 0, texture.get_height(), texture.get_height())))
 	return animation_frames
 
-TEXTURES: dict[str, list[pygame.Surface]] = {
+def load_character_animation(state):
+	textures = load_texture(state, 'Characters')
+	animation_frames = []
+	for i in range(int((pygame.Vector2(textures.get_width(), textures.get_height()) / PLAYER_SIZE.x).x)):
+		animation_frames.append(textures.subsurface((i * PLAYER_SIZE.x, 0, PLAYER_SIZE.x, PLAYER_SIZE.y)))
+	return animation_frames
+
+CHARACTER_TEXTURES: dict[str, list[pygame.Surface]] = {
+	f'{state.name}'.split('.')[0]: load_character_animation(state.name.split('.')[0]) for state in \
+		os.scandir(os.path.join('C:\\', *((__file__.split(':')[1]).split('\\')[:-1]), 'Assets', 'Images', 'Characters')) if state.name.split('.')[1] =='png'
+}
+
+TILE_TEXTURES: dict[str, list[pygame.Surface]] = {
 	'grass': load_tile_animation('grass'),
 	'water': load_tile_animation('water')
 }
